cfcdeb4a1b6c087b2aa212f3471461c3273824d7
xy520yh <7478059+xu_yan_111@user.noreply.gitee.com>
2020-06-10 16:41:49 +0800

    2

--- js/index.js
+++ js/index.js
@@ -1,105 +1,82 @@
-var phoneWidth =  parseInt(window.screen.width);
-var phoneScale = phoneWidth/640;
-var ua = navigator.userAgent;
-if (/Android (\d+\.\d+)/.test(ua)){
-	var version = parseFloat(RegExp.$1);
-	if(version>2.3){
-		document.write('<meta name="viewport" content="width=640, minimum-scale = '+phoneScale+', maximum-scale = '+phoneScale+', target-densitydpi=device-dpi">');
-	}else{
-		document.write('<meta name="viewport" content="width=640, target-densitydpi=device-dpi">');
-	}
-} else {
-	document.write('<meta name="viewport" content="width=640, user-scalable=no, target-densitydpi=device-dpi">');
+function rePosition(){
+    var width = document.documentElement.clientWidth || document.body.clientWidth;
+    var ratio = width/750;
+    if(width<=750){
+        document.getElementsByTagName('html')[0].style.fontSize=100*ratio+"px";
+    }else{
+        document.getElementsByTagName('html')[0].style.fontSize="100px";
+    }
 }
-if(RegExp("MicroMessenger").test(navigator.userAgent)){
-	document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-		WeixinJSBridge.call('hideToolbar');
-	});
-}
+rePosition();
+window.onresize = function(){
+    rePosition();
+};
+
 
 $(function(){
-	$('.video').click(function(){
-		$('#video-play').addClass('on');
-		$('.opa').addClass('on');
-		$('.video').css({'background':'none'});
-	});
-	
-	$('.opa').click(function(){
-		$('#video-play').removeClass('on');
-		$('.opa').removeClass('on');
-		$('.video').css({'background':'url(img/videobg.png?jv=1.0.20170710) no-repeat'});
-		
-	});
-	
-	$(".img_box li").click(function(e){
-		var index = $(this).index();
 
-		$(".swiper,.alert_bg2").show();
-		var mySwiper = new Swiper('.swiper-container2', {
-		pagination : '.swiper-pagination',
-		loop : true,
-		autoplayDisableOnInteraction: false,
-	    });
-        mySwiper.slideTo(index+1);
-	});
 	
-	$(".swiper").click(function(){
-		$(this).hide();
-	});
-	$(".swiper-container").click(function(e){
-		e.stopPropagation();
-	});
-	$('.alert_bg2').click(function(){
-		$('.alert_bg2,.swiper').hide();
-	});
-	(function(){
-		var roleswiper = new Swiper('.roles_wrap', {
-            autoplay:3000,
-            loop : true,
-            effect : 'fade',
-            onSlideChangeStart:function(swiper){
-                var length = $(".game_roles .swiper_dock_list li").length;
-				$(".game_roles .swiper_dock_list li img").removeClass('on');
-                $(".game_roles .swiper_dock_list li img").eq((swiper.activeIndex-1)%length).addClass('on');
-            }
-        });
-        $(".game_roles .swiper_dock_list li").click(function(){
-            var index = $(this).index()+1;
-            roleswiper.slideTo(index);
-			$(".game_roles .swiper_dock_list li img").removeClass('on');
-            $(".game_roles .swiper_dock_list li img").eq(index-1).addClass('on');
-        });
-	})();
-	(function(){
-		var animalswiper = new Swiper('.animal_wrap', {
-			autoplay:3000,
-			loop : true,
-			onSlideChangeStart:function(swiper){
-				$(".animal_wrap .swiper_dock_list li img").removeClass('on');
-				$(".animal_wrap .swiper_dock_list li img").eq((swiper.activeIndex-1)%$(".animal_wrap .swiper_dock_list li").length).addClass('on');
-			}
-		});
-		$(".animal_wrap .swiper_dock_list li").click(function(){
-			var index = $(this).index()+1;
-			animalswiper.slideTo(index);
-			$(".animal_wrap .swiper_dock_list li img").removeClass('on');
-			$(".animal_wrap .swiper_dock_list li img").eq(index-1).addClass('on');
-		});	
-	})();
-	(function(){
-		var clothesswiper = new Swiper('.clothes_wrap', {
-			autoplay:3000,
-			loop : true,
-			onSlideChangeStart:function(swiper){
-				$(".clothes_wrap .swiper_dock_list li img").removeClass('on');
-			    $(".clothes_wrap .swiper_dock_list li img").eq((swiper.activeIndex-1)%$(".clothes_wrap .swiper_dock_list li").length).addClass('on');
-			}
-		});
-		$(".clothes_wrap .swiper_dock_list li").click(function(){
-			var index = $(this).index()+1;
-			clothesswiper.slideTo(index);
-			$(".clothes_wrap .swiper_dock_list li img").removeClass('on');
-			$(".clothes_wrap .swiper_dock_list li img").eq(index-1).addClass('on');
-		});	
-	})();
+	$(function(){  
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body').animate({
+          scrollTop: targetOffset
+        },
+        1000);
+        return false;
+      }
+    }
+  });
+})
+
+
+
+	
 });
+$(function () {
+    $("#playbtn").click(function () {
+        $("#bg").show();
+        $("#mp_play").show();
+		//$("#iframe")[0].src="https://v.qq.com/iframe/player.html?vid=i0530szu3jz&tiny=0&auto=1";
+		$("#iframe")[0].src="https://v.qq.com/txp/iframe/player.html?vid=x0760htp3kp";
+    });
+	
+
+    $("#bg,#closebtn").click(function () {
+        initClose();
+    });
+
+});
+function initClose() {
+    $("#bg").hide();
+    $("#mp_play").hide();
+	$("#iframe")[0].src="";
+}
+
+
+
+
+
+
+
+$(function () {
+    $(".login_btn img").click(function () {
+        $("#bg").show();
+        $("#mp_play1").show();
+    });
+	
+
+    $("#bg,#closebtn1").click(function () {
+        initClose1();
+    });
+
+});
+function initClose1() {
+    $("#bg").hide();
+    $("#mp_play1").hide();
+	
+}
